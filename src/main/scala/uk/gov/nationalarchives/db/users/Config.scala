e106240d5a549981d9b25492ff258e62605005ad
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-03-25 15:18:19 +0000

    Pass the endpoint to the kms client

--- src/main/scala/uk/gov/nationalarchives/db/users/Config.scala
+++ src/main/scala/uk/gov/nationalarchives/db/users/Config.scala
@@ -7,14 +7,14 @@ import uk.gov.nationalarchives.aws.utils.Clients.kms
 import uk.gov.nationalarchives.aws.utils.KMSUtils
 
 object Config {
-  case class LambdaConfig(driver: String, username: String, password: String, url: String, consignmentApiUser: String, migrationsUser: String, functionName: String)
+  case class LambdaConfig(driver: String, username: String, password: String, url: String, consignmentApiUser: String, migrationsUser: String, functionName: String, kmsEndpoint: String)
 
   val lambdaConfig: LambdaConfig = ConfigSource.default.load[LambdaConfig] match {
     case Left(error) => throw new RuntimeException(error.prettyPrint(0))
     case Right(value) => value
   }
 
-  val kmsUtils: KMSUtils = KMSUtils(kms, Map("LambdaFunctionName" -> lambdaConfig.functionName))
+  val kmsUtils: KMSUtils = KMSUtils(kms(lambdaConfig.kmsEndpoint), Map("LambdaFunctionName" -> lambdaConfig.functionName))
 
   implicit val session: AutoSession.type = AutoSession
   Class.forName(lambdaConfig.driver)
