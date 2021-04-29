4b6be2ef0c42d89dce48c2b3c309941a6aa7f017
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-29 18:10:29 +0100

    Various fixes
    The CREATE EXTENSION call needs to be here because it needs a superuser
    to run it and this is the only time we run with one.
    
    Then create the migrations user first. Then switch roles to the
    migrations user to grant permissions to the consignment api user so it
    can use the tables when the database migrations are run.

--- src/main/scala/uk/gov/nationalarchives/db/users/Lambda.scala
+++ src/main/scala/uk/gov/nationalarchives/db/users/Lambda.scala
@@ -34,17 +34,24 @@ class Lambda {
   }
 
   def createConsignmentApiUsers: Boolean = {
+    //Create the extension. This needs to be done by the admin user and this is the only script run by the admin user.
+    val uuid = sqls.createUnsafely("uuid-ossp")
+    sql"""CREATE EXTENSION IF NOT EXISTS "$uuid" ;""".execute().apply()
+
+    val migrationsUser = createConsignmentApiUser(lambdaConfig.migrationsUser)
+    sql"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $migrationsUser;".execute.apply()
+    sql"GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $migrationsUser;".execute.apply()
+
     val apiUser = createConsignmentApiUser(lambdaConfig.consignmentApiUser)
+
+    //Switch to migrations user to set permissions on it's own tables correctly
+    sql"SET ROLE $migrationsUser".execute().apply()
     sql"GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO $apiUser;".execute.apply()
-    sql"GRANT USAGE on consignment_sequence_id to $apiUser;".execute.apply()
+    sql"GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $apiUser;".execute.apply()
 
     //Grants permissions for any new tables that are created.
     //This is not needed for the migrations user as it will be creating the tables so it will own them/
     sql"ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO $apiUser;".execute().apply()
-
-    val migrationsUser = createConsignmentApiUser(lambdaConfig.migrationsUser)
-    sql"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $migrationsUser;".execute.apply()
-    sql"GRANT ALL PRIVILEGES ON consignment_sequence_id TO $migrationsUser;".execute.apply()
   }
 
   def grantConnectAndUsage(user: SQLSyntax, database: SQLSyntax) = {
@@ -54,9 +61,10 @@ class Lambda {
 
   def createConsignmentApiUser(username: String): SQLSyntax = {
     //createUnsafely is needed as the usual interpolation returns ERROR: syntax error at or near "$1"
-    //There is a similar issue here https://github.com/scalikejdbc/scalikejdbc/issues/320
+    //    There is a similar issue here https://github.com/scalikejdbc/scalikejdbc/issues/320
     val user = sqls.createUnsafely(username)
-    sql"CREATE USER $user".execute().apply()
+    val password = sqls.createUnsafely("password")
+    sql"CREATE USER $user WITH PASSWORD '$password'".execute().apply()
     grantConnectAndUsage(user, sqls.createUnsafely("consignmentapi"))
     sql"GRANT rds_iam TO $user;".execute().apply()
     user
