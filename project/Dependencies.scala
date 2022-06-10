c85546212093d5256f35d40983755258c38fa9a2
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-06-10 08:23:03 +0000

    Update postgresql to 42.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.33"
-  lazy val postgres = "org.postgresql" % "postgresql" % "42.3.6"
+  lazy val postgres = "org.postgresql" % "postgresql" % "42.4.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val scalikeJdbc = "org.scalikejdbc" %% "scalikejdbc" % "4.0.0"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1"
