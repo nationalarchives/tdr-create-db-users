1df4643bcac7fce28902b4e379148380e7251088
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-10-01 08:25:23 +0000

    Update scalatest to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.38"
   lazy val postgres = "org.postgresql" % "postgresql" % "42.5.0"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
   lazy val scalikeJdbc = "org.scalikejdbc" %% "scalikejdbc" % "4.0.0"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
