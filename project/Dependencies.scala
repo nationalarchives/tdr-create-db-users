72b47a889eb20a0ba7cc0aa7018d57a02461e67d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-07 08:29:56 +0000

    Update scalatest to 3.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.100"
   lazy val postgres = "org.postgresql" % "postgresql" % "42.6.0"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val scalikeJdbc = "org.scalikejdbc" %% "scalikejdbc" % "4.0.0"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
