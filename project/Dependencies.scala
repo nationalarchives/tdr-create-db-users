0a3bf949af3e46cee654ac5cf9564f28693e087c
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-28 08:24:46 +0000

    Update circe-core, circe-generic, ... to 0.14.7 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val circeVersion = "0.14.6"
+  private val circeVersion = "0.14.7"
 
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
