cc64400ba2aaf509f8c127a7dfbaa16c87833445
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-08-25 08:23:49 +0000

    Update postgresql to 42.7.4 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.202"
-  lazy val postgres = "org.postgresql" % "postgresql" % "42.7.3"
+  lazy val postgres = "org.postgresql" % "postgresql" % "42.7.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val scalikeJdbc = "org.scalikejdbc" %% "scalikejdbc" % "4.3.1"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.7"
