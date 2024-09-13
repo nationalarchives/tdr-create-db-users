8a41f8399053612a1b1d01d725d3760f2a9bd79a
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-09-13 08:25:57 +0000

    Update kms-utils to 0.1.205 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
-  lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.204"
+  lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % "0.1.205"
   lazy val postgres = "org.postgresql" % "postgresql" % "42.7.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val scalikeJdbc = "org.scalikejdbc" %% "scalikejdbc" % "4.3.1"
